917854441977ac8ad9c13eddd356446a8be92225
Sergey Khlybov <khlybov82@yandex.ru>
2025-02-04 11:42:39 +0300

    fist version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplateContent = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsWrapper = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+const generateCard = (name, imageUrl, onRemove) => {
+  const newCard = cardTemplateContent.cloneNode(true);
+  const removeBtn = newCard.querySelector(".card__delete-button");
+
+  newCard.querySelector(".card__title").textContent = name;
+  const imageElement = newCard.querySelector(".card__image");
+  imageElement.src = imageUrl;
+  imageElement.alt = name;
+
+  removeBtn.addEventListener("click", onRemove);
+
+  cardsWrapper.append(newCard);
+};
 
 // @todo: Функция удаления карточки
+const removeCard = (event) => {
+  const cardToDelete = event.target.closest(".places__item.card");
+  if (cardToDelete) {
+    cardToDelete.remove();
+  }
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(({ name, link }) => generateCard(name, link, removeCard));
